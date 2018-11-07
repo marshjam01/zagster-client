53510173b53abddfc9b62da4524926f1d1acb5fb
J.P. Marshall <marshjam@oregonstate.edu>
2018-11-07 06:40:55 -0800

    attempt to add ride count visual

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
+const BASE_URL = "https://zagster-service.herokuapp.com"
 $(updateView)
-
-function updateView(){
-    alert("I hit the CTRL key but I'm still not in control! ")
+function updateView() {
+  alert('This is awsome!')
+  $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
+}
+function updateRideCount(data) {
+  numberOfRides = data.count
+  $("h2#rideCount").html(numberOfRides)
 }
