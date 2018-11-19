6589d1eb8a4b50cd8ff730cbb6b6e75efddec7c2
J.P. Marshall <marshjam@oregonstate.edu>
2018-11-18 16:41:48 -0800

    added chart js

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 const BASE_URL = "https://zagster-service.herokuapp.com"
+
 $(updateView)
+
 function updateView() {
-  $.getJSON(BASE_URL + "/rides/count", updateRideCount)
+    $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
+    $.getJSON(BASE_URL + "/rides/count/per_month" , printData )
 }
+
 function updateRideCount(data) {
-  numberOfRides = data.count
-  $("h2#rideCount").html(numberOfRides)
+    $("h2#count").html(data.count);
+    $("h2#count").css('visibility', 'visible').hide().fadeIn("slow");
 }
 
-
+function printData(data) {
+    console.log(data)
+}
 
