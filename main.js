1cff570a4b36b6a99930b1487dec4cc7c889e202
J.P. Marshall <marshjam@oregonstate.edu>
2018-11-25 21:43:01 -0800

    added data to chart

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 const BASE_URL = "https://zagster-service.herokuapp.com"
 
+
+const G5_TC = "/rides/count/g5"
+const COL_TC = "/rides/count/columbia_simpson"
+const DRAKE_TC = "/rides/count/drake_park"
+const GALV_TC = "/rides/count/galveston"
+const GRC_TC = "/rides/count/grc"
+const OLD_TC = "/rides/count/old_mill"
+const OSU_TC = "/rides/count/osu_cascades"
+const TEN_TC = "/rides/count/ten_barrel"
+
 $(updateView)
 
 function updateView() {
-    $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
-    $.getJSON(BASE_URL + "/rides/count/per_month" , printData )
+    $.getJSON(BASE_URL + G5_TC, updateRideCount)
+    $.getJSON(BASE_URL + G5_TC, printData)
 }
 
 function updateRideCount(data) {
